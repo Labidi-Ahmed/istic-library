2a75cd82673323a3b6a676881f73820e30032984
Labidi-Ahmed <jackitunisi@gmail.com>
2025-04-02 02:52:54 +0100

    finished client setup

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center text-xl font-bold justify-center min-h-svh">
+      hello world
+    </div>
+  );
+}
+
+export default App;
